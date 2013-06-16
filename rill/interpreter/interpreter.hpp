3d83dca03104ca3b6bd16f1f418ce4588c084f00
yutopp <yutopp@gmail.com>
2013-06-16 22:47:02 +0900

    applied Boost Software License V1.0

--- rill/interpreter/interpreter.hpp
+++ rill/interpreter/interpreter.hpp
@@ -1,3 +1,10 @@
+//
+// Copyright yutopp 2013 - .
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef RILL_INTERPRETER_HPP
 #define RILL_INTERPRETER_HPP
 
