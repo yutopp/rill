a799fd172764bdf11883ba80900bcb2a36c26588
yutopp <yutopp@gmail.com>
2013-10-31 16:21:01 +0900

    add support for gcc(<4.8.1) and clang(<3.1)

--- rill/config/macros.hpp
+++ rill/config/macros.hpp
@@ -9,16 +9,22 @@
 #ifndef RILL_CONFIG_MACROS_HPP
 #define RILL_CONFIG_MACROS_HPP
 
-#ifdef _MSC_VER
+// if only using GCC(>= 4.8.1) or clang(>=3.1), use specifier.
+#ifndef _MSC_VER
+# define RILL_MACRO_USE_FINAL_AND_OVERRIDE
+#elif __GNUC__ >= 4 && __GNUC_MINOR__ >= 8 && __GNUC_PATCHLEVEL__ >= 1
+# define RILL_MACRO_USE_FINAL_AND_OVERRIDE
+#elif __clang_major__ >= 3 && __clang_minor__ >= 1
+# define RILL_MACRO_USE_FINAL_AND_OVERRIDE
+#endif
 
+#ifdef RILL_MACRO_USE_FINAL_AND_OVERRIDE
+# define RILL_CXX11_FINAL      final
+# define RILL_CXX11_OVERRIDE   override
+#else
 # define RILL_CXX11_FINAL
 # define RILL_CXX11_OVERRIDE
-
-#else
-
-# define RILL_CXX11_FINAL final
-# define RILL_CXX11_OVERRIDE override
-
 #endif
+
 
 #endif /*RILL_CONFIG_MACROS_HPP*/
