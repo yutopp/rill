e76c25fcbbafdb1063047c4c08500056d425b8ad
yutopp <yutopp@gmail.com>
2016-07-12 00:46:26 +0900

    fix commands for coverage

--- test/run.sh
+++ test/run.sh
@@ -9,4 +9,4 @@ omake COVERAGE=true
 omake test_all
 
 # bisect-ppx-report -I rillc/src/ -html /artifacts/coverage/ test/bisect-*.out
-ocveralls --prefix rillc/src/ test/bisect-*.out --send
+ocveralls --repo_token $COVERALLS_REPO_TOKEN --send test/bisect-*.out --prefix rillc/src --send --git
