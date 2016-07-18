d9fc6666f3aab13fb32d912a1af8f42c126cbcc6
yutopp <yutopp@gmail.com>
2016-07-18 13:17:15 +0900

    [WIP] add unittest dir

---
+++ rillc/test/semantics_test.ml
@@ -0,0 +1,13 @@
+open OUnit2
+
+let test_test_1 test_ctxt =
+  assert_equal true true
+
+let rec suite =
+  "semantics_suite" >:::
+    [
+      "test_test_1" >:: test_test_1
+    ]
+
+let () =
+  run_test_tt_main suite
