ef6a3dfaea35c78ecfd07c99939441a768e361ed
yutopp <yutopp@gmail.com>
2020-06-08 21:27:15 +0900

    Move previous test codes into _migrate

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
-set -e -o pipefail -o xtrace
 
-eval `opam config env`
-ls -la
+set -eux -o pipefail
 
-omake test
+LLC=${RILL_LLC:-llc}
+CC=${RILL_CC:-gcc}
+
+echo $RILLC_COMPILER
+echo $RILLC_CORELIB_SRCDIR
+echo $RILLC_CORELIB_LIBDIR
+
+FILE=../rillc/test/simple.rill
+
+OUT_DIR=`mktemp -d '/tmp/rillc.XXXXXXXXXXXXXXXX'`
+
+# Emit an LLVM IR bitcode
+$RILLC_COMPILER \
+    --corelib_srcdir="$RILLC_CORELIB_SRCDIR" \
+    --out_dir="$OUT_DIR" \
+    "$FILE"
+
+FILE_BC_PATH="$OUT_DIR/$(basename $FILE).bc"
+FILE_ASM_PATH="$OUT_DIR/$(basename $FILE).s"
+FILE_OUT_PATH="$OUT_DIR/$(basename $FILE).out"
+
+# Emit an asm file
+$LLC --relocation-model=pic "$FILE_BC_PATH" -o "$FILE_ASM_PATH"
+
+# Emit an executable
+$CC -v -fPIE -L"$RILLC_CORELIB_LIBDIR" -static -lcore-c "$FILE_ASM_PATH" -o "$FILE_OUT_PATH"
+
+$FILE_OUT_PATH
