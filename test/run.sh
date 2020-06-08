980e867e330e8c5d3cf2623a721f664fab1c9732
yutopp <yutopp@gmail.com>
2020-06-09 05:16:42 +0900

    Move test files

--- test/run.sh
+++ test/run.sh
@@ -5,28 +5,66 @@ set -eux -o pipefail
 LLC=${RILL_LLC:-llc}
 CC=${RILL_CC:-gcc}
 
-echo $RILLC_COMPILER
-echo $RILLC_CORELIB_SRCDIR
-echo $RILLC_CORELIB_LIBDIR
+_=$RILLC_COMPILER
+_=$RILLC_CORELIB_SRCDIR
+_=$RILLC_CORELIB_LIBDIR
 
-FILE=../rillc/test/simple.rill
+TEST_PASS_DIR="../test/pass"
 
-OUT_DIR=`mktemp -d '/tmp/rillc.XXXXXXXXXXXXXXXX'`
+function failed_to_execute() {
+    echo "Failed to execute: $1 in case '$2'" >2
+    exit 1
+}
 
-# Emit an LLVM IR bitcode
-$RILLC_COMPILER \
-    --corelib_srcdir="$RILLC_CORELIB_SRCDIR" \
-    --out_dir="$OUT_DIR" \
-    "$FILE"
+function build_and_execute() {
+    local CASENAME="$1"
+    local FILE="$TEST_PASS_DIR/$CASENAME.rill"
+    local TEST_EXPECT_FILE="$TEST_PASS_DIR/$CASENAME.expect"
 
-FILE_BC_PATH="$OUT_DIR/$(basename $FILE).bc"
-FILE_ASM_PATH="$OUT_DIR/$(basename $FILE).s"
-FILE_OUT_PATH="$OUT_DIR/$(basename $FILE).out"
+    OUT_DIR=`mktemp -d '/tmp/rillc.XXXXXXXXXXXXXXXX'`
 
-# Emit an asm file
-$LLC --relocation-model=pic "$FILE_BC_PATH" -o "$FILE_ASM_PATH"
+    # Emit an LLVM IR bitcode
+    $RILLC_COMPILER \
+        --corelib_srcdir="$RILLC_CORELIB_SRCDIR" \
+        --out_dir="$OUT_DIR" \
+        "$FILE" || failed_to_execute "compile" "$CASENAME"
 
-# Emit an executable
-$CC -v -fPIE -L"$RILLC_CORELIB_LIBDIR" -static -lcore-c "$FILE_ASM_PATH" -o "$FILE_OUT_PATH"
+    FILE_BC_PATH="$OUT_DIR/$(basename $FILE).bc"
+    FILE_ASM_PATH="$OUT_DIR/$(basename $FILE).s"
+    FILE_OUT_PATH="$OUT_DIR/$(basename $FILE).out"
 
-$FILE_OUT_PATH
+    # Emit an asm file
+    $LLC --relocation-model=pic "$FILE_BC_PATH" -o "$FILE_ASM_PATH" \
+        || failed_to_execute "asm: $LLC" "$CASENAME"
+
+    # Emit an executable
+    $CC -v -fPIE \
+        -L"$RILLC_CORELIB_LIBDIR" \
+        -static \
+        -lcore-c \
+        "$FILE_ASM_PATH" \
+        -o "$FILE_OUT_PATH" \
+        || failed_to_execute "exec: $CC" "$CASENAME"
+
+    # Execute
+    local OUTPUT=$($FILE_OUT_PATH 2>&1)
+    local EXIT_CODE=$?
+    if [ ! $EXIT_CODE -eq 0 ]; then
+        failed_to_execute "a.out(exitcode: $EXIT_CODE)" "$CASENAME"
+    fi
+    local TEST_ACTUAL_FILE="$OUT_DIR/test.actual"
+    echo "$OUTPUT" > "$TEST_ACTUAL_FILE"
+
+    # Expect file
+    if [ ! -f "$TEST_EXPECT_FILE" ]; then
+       failed_to_execute "expect: file not found ($TEST_EXPECT_FILE)" "$CASENAME"
+    fi
+
+    diff -u "$TEST_ACTUAL_FILE" "$TEST_EXPECT_FILE" \
+        || failed_to_execute "expect" "$CASENAME"
+}
+
+for path in "$TEST_PASS_DIR"/*.rill; do
+    casename="$(basename "$path" .rill)"
+    build_and_execute "$casename"
+done
