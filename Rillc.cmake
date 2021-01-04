f4a8296be5f05cf66c3dc0dc521696365a249479
yutopp <yutopp@gmail.com>
2021-01-05 02:56:10 +0900

    Use rillc as external package

---
+++ Rillc.cmake
@@ -0,0 +1,20 @@
+#
+# Base compiler: rillc
+# Rillc will be installed in the following directory structure.
+#
+# - ${PREFIX}/bin/rillc-*
+# - ${PREFIX}/doc/rillc/*
+#
+include(ExternalProject)
+ExternalProject_Add(
+  rillc
+  BUILD_COMMAND make build
+  INSTALL_COMMAND make PREFIX=${CMAKE_BINARY_DIR} install
+  SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/rillc"
+  BUILD_IN_SOURCE On
+  BUILD_ALWAYS On
+  DOWNLOAD_COMMAND ""
+  CONFIGURE_COMMAND ""
+  LOG_BUILD On
+  )
+
