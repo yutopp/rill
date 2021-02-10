bd730c38aa8b628228f0cb5493149d6c3ed1cb2b
yutopp <yutopp@gmail.com>
2021-02-10 23:39:15 +0900

    Fix tests

--- rillc/lib/rir/tests/module.ml
+++ rillc/lib/rir/tests/module.ml
@@ -6,4 +6,12 @@
  * http://www.boost.org/LICENSE_1_0.txt)
  *)
 
-let () = ()
+let test_empty_module () =
+  let ctx = Rir.Context.create () in
+  let m = Rir.Module.create ~ctx in
+  ()
+
+let () =
+  let open Alcotest in
+  run "module"
+    [ ("create", [ test_case "empty module" `Quick test_empty_module ]) ]
